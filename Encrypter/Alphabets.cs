09b8f0ff6aa0c20cee16fee42db20cc4f6250228
Павел Присталов <pristalov.forken@mail.ru>
2021-01-31 10:57:20 +0400

    release version

---
+++ Encrypter/Alphabets.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encrypter
+{
+    class Alphabets
+    {
+        public static char[] Eng = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 
+            'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 
+            's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+
+        public static char[] Rus = { 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 
+            'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 
+            'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 
+            'ь', 'э', 'ю', 'я' };
+    }
+}
---
+++ Encrypter/MainWindow.xaml.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Encrypter
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Encrypt_Click(object sender, RoutedEventArgs e)
+        {
+            Encrypt();
+        }
+        private void Input_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+                Encrypt();
+            if (e.Key == Key.Escape)
+                Input.Text = "";
+        }
+
+        private void Toolbar_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            // Window move
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        private void Minimize_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void Encrypt()
+        {
+            // Get entered text
+            string text = Input.Text;
+            // Spliting into words 
+            string[] words = text.Split();
+            // Result
+            string encryptedText = "";
+
+            foreach (var word in words)
+            {
+                // Spliting into chars
+                char[] chars = word.ToCharArray();
+
+                int posInWord = 0;
+                foreach (var ch in chars)
+                {
+                    char[] curAlph;
+                    // Char to lower
+                    char curChar = ch.ToString().ToLower().ToCharArray()[0];
+
+                    if (Alphabets.Eng.Contains(curChar))
+                        curAlph = Alphabets.Eng;
+                    else if (Alphabets.Rus.Contains(curChar))
+                        curAlph = Alphabets.Rus;
+                    else
+                    {
+                        encryptedText += curChar;
+                        continue;
+                    }
+
+                    // Position in alphabet of current char
+                    int posInAlph = 0;
+                    while (curAlph[posInAlph] != curChar)
+                        posInAlph++;
+
+                    // Encrypting
+                    posInWord++;
+                    int offset = word.Length - posInWord;
+
+                    // Final position
+                    int finPos = Math.Abs(posInAlph - curAlph.Length) + offset;
+
+                    // If position bigger, than alphabet lenght
+                    while (finPos >= curAlph.Length)
+                        finPos -= curAlph.Length;
+
+                    // If the char was upper
+                    if (ch != curChar)
+                        encryptedText += curAlph[finPos].ToString().ToUpper().ToCharArray()[0];
+                    else
+                        encryptedText += curAlph[finPos];
+                }
+
+                encryptedText += ' ';
+            }
+
+            // Set encrypted text to output block
+            Output.Text = encryptedText.Trim();
+        }
+    }
+}
